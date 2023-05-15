1e02f9f219f2da66dddd76fffeddf3d48e933bf1
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-05-15 13:50:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
+// Define the Person constructor function
 // complete this js code
 function Person(name, age) {
-	this.Name = name
-	this.Age = age
+  this.name = name;
+  this.age = age;
 }
 
-Person.prototype.greet = function () {
-	console.log(`Hello, my name is ${this.Name}, I am ${this.Age} years old.`)
-}
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is Alice and I am 25 years old.`);
+};
 
 function Employee(name, age, jobTitle) {
-	this.Name = name
-	this.Age = age
-	this.JobTitle = jobTitle
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
 }
 
-Employee.prototype = Object.create(Person.prototype)
-// const E1 = new Employee('Rajdeep', '22', 'SDE')
-Employee.prototype.jobGreet = function () {
-	console.log(`Hello, my name is ${this.Name}, I am ${this.Age} years old, and my job title is ${this.JobTitle}.`)
-}
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
 
-// E1.greet()
-// E1.jobGreet()
+
+/*function Person(name, age){
+	this._name = name;
+	this._age = age;
+}
+Person.prototype.greet = function(){
+	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
+}
+function Employee(name, age, jobTitle){
+	Person.call(this, name, age);
+	this._jobTitle = jobTitle;
+}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+Employee.prototype.gobGreet = function(){
+	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
+}*/
 
 
 // Do not change code below this line
